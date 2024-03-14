3ef9820ab6192778fe84cf9fce10e292449ee8ca
Shubrotto Kumar Hawlader <skshubroto@gmail.com>
2024-03-14 15:46:39 +0600

    vite config add

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/psonal-app-portfolio/",
+  base: "/Psonal-app/",
   plugins: [react()],
   build: {
     OutDir: " build",
